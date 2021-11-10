a53b447b4207868f9c6031aeeb9d18f1bada8eee
koleahmantle <koleahmantle@gmail.com>
2021-11-09 21:46:19 -0500

    Add project files.

---
+++ FitnessApp.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FitnessApp.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FitnessApp.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FitnessApp.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
