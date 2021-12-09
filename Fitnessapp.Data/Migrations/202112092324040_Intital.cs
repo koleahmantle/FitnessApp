2eaa0379ebb62406a0c66264e63af20f087c71cb
William Wentworth <WWentworth@live.com>
2021-12-09 18:42:17 -0500

    a

--- Fitnessapp.Data/Migrations/202112092324040_Intital.cs
+++ Fitnessapp.Data/Migrations/202112092324040_Intital.cs
@@ -1,12 +1,50 @@
-namespace Fitnessapp.Data.Migrations
+﻿namespace Fitnessapp.Data.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class UserTracker : DbMigration
+    public partial class Intital : DbMigration
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Recipe",
+                c => new
+                    {
+                        RecipeId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        RecipeName = c.String(nullable: false),
+                        Diet = c.String(nullable: false),
+                        MealType = c.Double(nullable: false),
+                        Calories = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.RecipeId);
+            
+            CreateTable(
+                "dbo.UserTracker",
+                c => new
+                    {
+                        UserTrackerId = c.Int(nullable: false, identity: true),
+                        UserId = c.Guid(nullable: false),
+                        UserName = c.String(),
+                        TagLine = c.String(),
+                    })
+                .PrimaryKey(t => t.UserTrackerId);
+            
+            CreateTable(
+                "dbo.Workout",
+                c => new
+                    {
+                        WorkoutId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        Name = c.String(nullable: false),
+                        Category = c.String(nullable: false),
+                        Duration = c.Double(nullable: false),
+                        Intensity = c.String(nullable: false),
+                        CaloriesBurned = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.WorkoutId);
+            
             CreateTable(
                 "dbo.IdentityRole",
                 c => new
@@ -78,28 +116,6 @@ namespace Fitnessapp.Data.Migrations
                 .Index(t => t.ApplicationUser_Id);
             
             CreateTable(
-                "dbo.UserTracker",
-                c => new
-                    {
-                        UserTrackerId = c.Int(nullable: false, identity: true),
-                        UserId = c.Guid(nullable: false),
-                    })
-                .PrimaryKey(t => t.UserTrackerId);
-            
-            CreateTable(
-                "dbo.Workout",
-                c => new
-                    {
-                        WorkoutId = c.Int(nullable: false, identity: true),
-                        OwnerId = c.Guid(nullable: false),
-                        Name = c.String(nullable: false),
-                        Category = c.String(nullable: false),
-                        Duration = c.Double(nullable: false),
-                        Intensity = c.String(nullable: false),
-                    })
-                .PrimaryKey(t => t.WorkoutId);
-            
-            CreateTable(
                 "dbo.WorkoutUserTracker",
                 c => new
                     {
@@ -112,30 +128,49 @@ namespace Fitnessapp.Data.Migrations
                 .Index(t => t.Workout_WorkoutId)
                 .Index(t => t.UserTracker_UserTrackerId);
             
+            CreateTable(
+                "dbo.UserTrackerRecipe",
+                c => new
+                    {
+                        UserTracker_UserTrackerId = c.Int(nullable: false),
+                        Recipe_RecipeId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.UserTracker_UserTrackerId, t.Recipe_RecipeId })
+                .ForeignKey("dbo.UserTracker", t => t.UserTracker_UserTrackerId, cascadeDelete: true)
+                .ForeignKey("dbo.Recipe", t => t.Recipe_RecipeId, cascadeDelete: true)
+                .Index(t => t.UserTracker_UserTrackerId)
+                .Index(t => t.Recipe_RecipeId);
+            
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.WorkoutUserTracker", "UserTracker_UserTrackerId", "dbo.UserTracker");
-            DropForeignKey("dbo.WorkoutUserTracker", "Workout_WorkoutId", "dbo.Workout");
             DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.UserTrackerRecipe", "Recipe_RecipeId", "dbo.Recipe");
+            DropForeignKey("dbo.UserTrackerRecipe", "UserTracker_UserTrackerId", "dbo.UserTracker");
+            DropForeignKey("dbo.WorkoutUserTracker", "UserTracker_UserTrackerId", "dbo.UserTracker");
+            DropForeignKey("dbo.WorkoutUserTracker", "Workout_WorkoutId", "dbo.Workout");
+            DropIndex("dbo.UserTrackerRecipe", new[] { "Recipe_RecipeId" });
+            DropIndex("dbo.UserTrackerRecipe", new[] { "UserTracker_UserTrackerId" });
             DropIndex("dbo.WorkoutUserTracker", new[] { "UserTracker_UserTrackerId" });
             DropIndex("dbo.WorkoutUserTracker", new[] { "Workout_WorkoutId" });
             DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropTable("dbo.UserTrackerRecipe");
             DropTable("dbo.WorkoutUserTracker");
-            DropTable("dbo.Workout");
-            DropTable("dbo.UserTracker");
             DropTable("dbo.IdentityUserLogin");
             DropTable("dbo.IdentityUserClaim");
             DropTable("dbo.ApplicationUser");
             DropTable("dbo.IdentityUserRole");
             DropTable("dbo.IdentityRole");
+            DropTable("dbo.Workout");
+            DropTable("dbo.UserTracker");
+            DropTable("dbo.Recipe");
         }
     }
 }
